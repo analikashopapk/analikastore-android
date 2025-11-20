28ac505455ad5c977ce0cd348ba943e407a07748
analikashopapk <analikashop@gmail.com>
2025-11-20 15:32:32 +0530

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,3 +1,18 @@
-// Top-level build file (Kotlin DSL)
-plugins {
+// Top-level build.gradle.kts (minimal, ensures repositories available)
+buildscript {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+    dependencies {
+        // optional: you can add classpath plugins here if required
+    }
+}
+
+allprojects {
+    repositories {
+        google()
+        mavenCentral()
+    }
 }
