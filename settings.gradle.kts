7520e4da2c8f91b0c33be1eb7225532a258dbbec
analikashopapk <analikashop@gmail.com>
2025-11-21 14:01:15 +0530

    Update settings.gradle.kts

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -9,7 +9,13 @@ pluginManagement {
         id("com.android.application") version "8.2.1"
         id("org.jetbrains.kotlin.android") version "2.1.0" // or 1.9.x depending on your kotlin libs
         id("com.google.gms.google-services") version "4.4.0" // example
+       
     }
+plugins {
+    java
+    id("org.jetbrains.kotlin.jvm") version "1.9.0"
+}
+
 }
 
 dependencyResolutionManagement {
