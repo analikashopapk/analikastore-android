c7c7b8d33797ee708e984ad694dbc7d869df45b1
analikashopapk <analikashop@gmail.com>
2025-11-21 17:37:15 +0530

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,12 +1,6 @@
-// root build.gradle.kts
 plugins {
-    // Provide AGP (apply false so modules apply it)
     id("com.android.application") version "8.2.0" apply false
-
-    // Kotlin plugin (use version 2.1.0 to match libraries you have)
     id("org.jetbrains.kotlin.android") version "2.1.0" apply false
-
-    // Google services plugin for Firebase — version declared here
     id("com.google.gms.google-services") version "4.3.15" apply false
 }
 
