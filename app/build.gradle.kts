37af6c9d2269794e32875f9c8e8a863e736e36aa
analikashopapk <analikashop@gmail.com>
2025-11-21 09:23:43 +0530

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -22,8 +22,6 @@ android {
         }
         getByName("release") {
             isMinifyEnabled = false
-            // If you enable minify/proguard later, add proguard rules.
-            // proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
         }
     }
 
@@ -35,38 +33,29 @@ android {
     kotlinOptions {
         jvmTarget = "17"
     }
-
-    // packagingOptions or other settings can go here if needed
-}
-
-repositories {
-    // Project-level repositories are normally configured in settings.gradle.kts,
-    // but having these here is safe if your settings file doesn't set repositoriesMode=FAIL_ON_PROJECT_REPOS
-    google()
-    mavenCentral()
 }
 
 dependencies {
     // Firebase BOM
     implementation(platform("com.google.firebase:firebase-bom:34.6.0"))
 
-    // Firebase artifacts (no explicit versions; BOM controls them)
+    // Firebase artifacts
     implementation("com.google.firebase:firebase-messaging")
     implementation("com.google.firebase:firebase-analytics")
 
-    // Kotlin stdlib matching kotlin plugin
+    // Kotlin stdlib matching plugin
     implementation("org.jetbrains.kotlin:kotlin-stdlib:1.9.10")
 
-    // Lottie (if you use it)
+    // Lottie
     implementation("com.airbnb.android:lottie:5.2.0")
 
-    // Common Android libs used previously
+    // Core libraries
     implementation("androidx.core:core-ktx:1.10.1")
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.9.0")
     implementation("androidx.recyclerview:recyclerview:1.3.0")
 
-    // Networking / image libs (adjust if your project differs)
+    // Networking / image
     implementation("io.coil-kt:coil:2.4.0")
     implementation("com.squareup.retrofit2:retrofit:2.9.0")
     implementation("com.squareup.retrofit2:converter-gson:2.9.0")
