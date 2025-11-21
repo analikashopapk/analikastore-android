a69e831a9b21d953f628509ec3e5abe55b633439
analikashopapk <analikashop@gmail.com>
2025-11-21 16:13:31 +0530

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,9 +1,11 @@
 // Root build.gradle.kts
 plugins {
-    id("com.android.application") version "8.1.0" apply false       // <- replace with your AGP version
-    id("org.jetbrains.kotlin.android") version "1.9.10" apply false // <- replace with your Kotlin version
-    id("com.google.gms.google-services") version "4.4.0" apply false // <- replace with your Google services version
+    kotlin("jvm") version "2.1.0" apply false
+    kotlin("android") version "2.1.0" apply false
+    id("com.android.application") version "8.2.0" apply false
+    // ...
 }
+
 
 // Provide repositories so plugin resolution and subprojects can resolve dependencies
 buildscript {
