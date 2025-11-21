0171cc4508f87801767d54e066dc3fadf4436510
analikashopapk <analikashop@gmail.com>
2025-11-21 16:03:54 +0530

    Update settings.gradle.kts

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -14,11 +14,13 @@ pluginManagement {
 }
 
 dependencyResolutionManagement {
-    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositoriesMode.set(RepositoriesMode.PREFER_SETTINGS)
     repositories {
         google()
         mavenCentral()
     }
+}
+
 }
 rootProject.name = "analikastore-android"
 include(":app")
