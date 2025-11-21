54ad3705c21f7d7ab0e8a3729f209b9d6b3d251e
analikashopapk <analikashop@gmail.com>
2025-11-21 16:41:16 +0530

    Update settings.gradle.kts

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,21 +1,17 @@
-// settings.gradle.kts - replace your current file with this exact content
-
 pluginManagement {
     repositories {
         gradlePluginPortal()
         google()
         mavenCentral()
     }
-    // Optional: declare plugin versions here if you want centralized plugin versions.
 }
 
 dependencyResolutionManagement {
-    // Prefer repositories declared here (in settings) over repositories declared in build scripts
+    // Prefer settings repositories so modules don't need repositories blocks
     repositoriesMode.set(RepositoriesMode.PREFER_SETTINGS)
     repositories {
         google()
         mavenCentral()
-        // add others only if required, e.g. maven("https://jitpack.io")
     }
 }
 
