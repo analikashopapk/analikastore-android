77c527652bc52a481094505a1554e9fddbcbdadb
analikashopapk <analikashop@gmail.com>
2025-11-21 14:11:21 +0530

    Refactor plugin declarations in settings.gradle.kts

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -7,15 +7,10 @@ pluginManagement {
     plugins {
         // pick versions compatible with your Android Gradle Plugin and Kotlin libs
         id("com.android.application") version "8.2.1"
-        id("org.jetbrains.kotlin.android") version "2.1.0" // or 1.9.x depending on your kotlin libs
-        id("com.google.gms.google-services") version "4.4.0" // example
-       
+        id("org.jetbrains.kotlin.android") version "2.1.0"
+        id("org.jetbrains.kotlin.jvm") version "2.1.0"
+        id("com.google.gms.google-services") version "4.4.0"
     }
-plugins {
-    java
-    id("org.jetbrains.kotlin.jvm") version "2.1.0"
-}
-
 }
 
 dependencyResolutionManagement {
