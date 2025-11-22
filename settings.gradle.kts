d3df8de742161f0b6d43f4a65d5fbd4665963a21
analikashopapk <analikashop@gmail.com>
2025-11-22 11:41:45 +0530

    Update settings.gradle.kts

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,9 +1,18 @@
 // settings.gradle.kts
 pluginManagement {
     repositories {
+        // plugin resolution repositories (order matters)
         gradlePluginPortal()
         google()
         mavenCentral()
+    }
+
+    // Declare plugin versions here so module build files can use plugins { id(...) } without a version
+    plugins {
+        // use AGP version compatible with your Gradle; adjust version if you need a different AGP
+        id("com.android.application") version "8.2.0" apply false
+        id("com.android.library")     version "8.2.0" apply false
+        id("org.jetbrains.kotlin.android") version "1.9.20" apply false
     }
 }
 
